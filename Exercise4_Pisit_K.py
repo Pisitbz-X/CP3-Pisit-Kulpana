df52c4ff8de9710922bfc52364f3fd8a5254ef6c
Pisitbz-X <pisitbz.info@gmail.com>
2020-07-30 01:03:55 +0700

    Finish

---
+++ Exercise4_Pisit_K.py
@@ -0,0 +1,25 @@
+print("-------------------------------------------------------")
+print("Score announcement on the first semester of second year")
+print("-------------------------------------------------------")
+
+## Variable 4 subject 
+FES = 74.5      #FES = Foundation English score
+GBS = 86.25     #GBS = General Business score
+ICS = 99.5      #ICS = Introduction to Computer System score
+CPS = 93        #CPS = Computer Programming score
+
+## Add score
+print("- Foundation English :", FES,"/ 100")
+print("- General Business :",GBS,"/ 100")
+print("- Introduction to Computer System :", ICS,"/ 100")
+print("- Computer Programming :",CPS,"/ 100")
+print("-------------------------------------------------------")
+
+print("Total :",FES+GBS+ICS+CPS,"/ 400")
+
+
+    
+
+
+
+
